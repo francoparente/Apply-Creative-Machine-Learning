a12e73aadf64f6d23e8c72ee305520b39bb886b5
Franco Parente <fparente14@frba.utn.edu.ar>
2021-12-19 14:27:57 -0300

    create data set

--- js/script.js
+++ js/script.js
@@ -2,3 +2,27 @@ var rapidLib = RapidLib();
 var myRegression = new rapidLib.Regression();
 var myClassification = new rapidLib.Classification();
 var mySeriesClassification = new rapidLib.SeriesClassification();
+
+var myData = [
+    {
+      input:  [48],
+      output: [130.81]
+    },
+    {
+      input:  [54],
+      output: [185.00]
+    },
+    {
+      input:  [60],
+      output: [261.63]
+    },
+    {
+      input:  [66],
+      output: [369.994]
+    },
+    {
+      input:  [72],
+      output: [523.25]
+    }
+];
+
