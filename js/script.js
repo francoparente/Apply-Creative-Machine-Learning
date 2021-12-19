aaf9522af3aac686781122ed847787a7b7593660
Franco Parente <fparente14@frba.utn.edu.ar>
2021-12-19 14:26:00 -0300

    create a RapidLib object

---
+++ js/script.js
@@ -0,0 +1,4 @@
+var rapidLib = RapidLib();
+var myRegression = new rapidLib.Regression();
+var myClassification = new rapidLib.Classification();
+var mySeriesClassification = new rapidLib.SeriesClassification();
